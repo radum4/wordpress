12b9d3b9ed8a15ffbae5ebb9309334530839b274
Radu Mitroi <ram@radum.org>
2015-06-11 21:38:59 +0300

    Fixed permissions

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#yum groupinstall -y "Web Server" "MySQL Database" "PHP Support"
-yum groupinstall -y "Web Server" "MySQL Server" "PHP Support"
+yum groupinstall -y "Web Server" "MySQL Database" "PHP Support"
+#yum groupinstall -y "Web Server" "MySQL Server" "PHP Support"
 yum install -y php-mysql
